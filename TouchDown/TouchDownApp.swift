072c726cc605ac30c448c4575b79c7f5df80fc2a
Benjamin <dotsehyde@gmail.com>
2022-06-21 18:28:40 +0000

    Init

---
+++ TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by Benjamin on 21/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
